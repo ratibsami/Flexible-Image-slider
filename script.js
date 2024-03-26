5a468f4c17cf0c1ece209a665adb3e8986ca771e
Rateb Sami <137919504+ratibsami@users.noreply.github.com>
2024-03-26 14:53:48 +0430

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+//RatebSami©️copyright 
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function moveSlide(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("slide");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
+var slideInterval = setInterval(moveSlide, 5000, 1); //Time 5sec images transfer
+
